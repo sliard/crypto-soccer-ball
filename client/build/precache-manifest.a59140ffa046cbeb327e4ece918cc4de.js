64e629458ae2473e60ff2cf96a544a4aade330fa
Liard Samuel <samuel.liard@orange.com>
2021-04-05 14:31:48 +0200

    add build

---
+++ client/build/precache-manifest.a59140ffa046cbeb327e4ece918cc4de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7051495352b017843af722b210df196b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f6a8350e5d1656d059b",
+    "url": "/static/css/2.a6d1c13d.chunk.css"
+  },
+  {
+    "revision": "0f58af146c7f8955a6d9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4f6a8350e5d1656d059b",
+    "url": "/static/js/2.dcf4ebbd.chunk.js"
+  },
+  {
+    "revision": "0f58af146c7f8955a6d9",
+    "url": "/static/js/main.b4dbab34.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
