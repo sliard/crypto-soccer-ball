ed7010f698e2d1addac6af2b72b8614dbbcb1151
Liard Samuel <samuel.liard@orange.com>
2021-04-05 14:15:01 +0200

    fix ropsten url

--- truffle-config.js
+++ truffle-config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://mainnet.infura.io/v3/${process.env.API_KEY}`)
+        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://ropsten.infura.io/v3/${process.env.API_KEY}`)
       },
       network_id: 3,
       gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
